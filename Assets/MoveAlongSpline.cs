9785cec53f06fb1f69b383cc794e03b69793edd9
viktor boburka <viktor@boburka.cz>
2025-07-11 17:25:41 +0200

    moving along spline with key

---
+++ Assets/MoveAlongSpline.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class MoveAlongSpline : MonoBehaviour
+{
+
+    [SerializeField] KeyCode key;
+    bool lastKeyPressed;
+    float progress = 0f;
+
+
+    float maxSpeed = 0.02f;
+    float minSpeed = 0.005f;
+    float speed = 0f;
+    float acceleration = 0.005f;
+    SplineAnimate splineAnimate;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        splineAnimate = GetComponent<SplineAnimate>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool currentKeyPress = Input.GetKey(key);
+        if (currentKeyPress) {
+            if (!lastKeyPressed) {
+                speed = 0f;
+            }
+            speed += acceleration * Time.deltaTime;
+            speed = Mathf.Clamp(speed, minSpeed, maxSpeed);
+
+            progress += speed;
+        }
+        else {
+            if (lastKeyPressed) {
+                speed = 0f;
+            }
+            speed += acceleration * Time.deltaTime;
+            speed = Mathf.Clamp(speed, minSpeed, maxSpeed);
+
+            progress -= speed;
+        }
+        progress = Mathf.Clamp(progress, 0f, 0.999f);
+
+        splineAnimate.ElapsedTime = progress;
+
+        lastKeyPressed = Input.GetKey(key);
+    }
+}
