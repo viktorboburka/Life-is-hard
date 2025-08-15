1b3ad0794f01db3200e106635fe51f37745ddeef
viktor boburka <viktor@boburka.cz>
2025-08-15 14:15:15 +0200

    post card flipper differentiate between intro text and outro text

--- Assets/PostCardFlipper.cs
+++ Assets/PostCardFlipper.cs
@@ -6,7 +6,8 @@ public class PostCardFlipper : MonoBehaviour
 
     [SerializeField] Sprite front;
     [SerializeField] Sprite back;
-    [SerializeField] GameObject backWriting;
+    [SerializeField] GameObject backWritingIntro;
+    [SerializeField] GameObject backWritingOutro;
     [SerializeField] AudioSource flipSound;
     SpriteRenderer sr;
     [SerializeField] float backWritingDuration = 10f;
@@ -28,36 +29,38 @@ public class PostCardFlipper : MonoBehaviour
         flipSound.Play();
         DOVirtual.DelayedCall(flipDuration / 2f, () =>
         {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+
             if (sr.sprite == front)
             {
                 sr.sprite = back;
                 //TODO: only show old writing
-                backWriting.SetActive(true);
+                backWritingIntro.SetActive(true);
             }
             else
             {
                 sr.sprite = front;
-                backWriting.SetActive(false);
+                backWritingIntro.SetActive(false);
             }
 
-            foreach (Transform child in transform)
-            {
-                if (child.gameObject != backWriting)
-                    child.gameObject.SetActive(false);
-            }
         });
         transform.DORotate(new Vector3(0, 180, 0), flipDuration).SetEase(Ease.InOutSine).OnComplete(() =>
         {
             flipping = false;
         });
 
-        DOVirtual.DelayedCall(flipDuration * 2, () => {
+        DOVirtual.DelayedCall(flipDuration * 2, () =>
+        {
             if (sr.sprite == back)
             {
-                //TODO: show new writing
+                backWritingOutro.SetActive(true);
             }
             else
             {
+                backWritingOutro.SetActive(false);
             }
         });
     }
@@ -67,7 +70,8 @@ public class PostCardFlipper : MonoBehaviour
         return flipDuration;
     }
 
-    public float GetWritingDuration() {
+    public float GetWritingDuration()
+    {
         return backWritingDuration;
     }
 
